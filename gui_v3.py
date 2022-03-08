f853e277caf0395f952cfa2dcee4c705b886ddcc
mpierangeli <mpierangeli@estudiantes.unsam.edu.ar>
2022-03-07 23:21:12 -0300

    agrego funciones al gui

--- gui_v3.py
+++ gui_v3.py
@@ -1,3 +1,4 @@
+from itertools import count
 from tkinter import *
 from PIL import Image,ImageTk
 from tkinter import filedialog
@@ -8,18 +9,12 @@ def draw_line(event):
         x1, y1 = cv.old_coords
         cv.create_line(x, y, x1, y1,tags="dibujos")
     cv.old_coords = x,y
+
 def clear_app(event):
     cv.old_coords = None
+
 def canvas_clear():
     cv.delete("dibujos")
-    #img_selector()
-""" def img_selector():
-    global ima
-    global filepath
-    filepath = filedialog.askopenfilename()
-    ima = Image.open(filepath)
-    ima_res = ImageTk.PhotoImage(ima.resize((50,50),Image.ANTIALIAS))
-    Label(l_frame, image=ima_res).grid(row=2, column=0, padx=5, pady=5) """
 
 def img_selector(num):
     global pepe
@@ -29,7 +24,19 @@ def img_selector(num):
         pepe = ImageTk.PhotoImage(Image.open("images\\bordes.png"))
     cv.itemconfig(cv_ima,image=pepe)
 
+
+def zoom_app(event):
+    global zoom
+    zoom+=round(event.delta/120)
+    if zoom<1:
+        zoom = 1
+    print(f"zoom: {zoom}")
+
+
+
+
 #MAIN WINDOW SETUP
+zoom = 1
 root = Tk()
 root.title("Software de Prueba PEFI 2022")
 root.maxsize(800, 600)
@@ -54,13 +61,11 @@ cv.old_coords = None
 
 cv.bind('<B1-Motion>', draw_line)
 cv.bind('<ButtonRelease-1>', clear_app)
-
+cv.bind("<MouseWheel>", zoom_app)
 
 
 tool_bar = Frame(l_frame, width=180, height=340)
 tool_bar.grid(row=2, column=0, padx=5, pady=5)
-Label(tool_bar, text="Tools").grid(row=0, column=0, padx=5, pady=10)
-#Label(tool_bar, text="Filters").grid(row=0, column=3, padx=5, pady=10)
 Button(tool_bar, text="Clear",command = canvas_clear).grid(row=2, column=0, padx=5, pady=5)
 Button(tool_bar, text="Insert Image",command = lambda: img_selector(1)).grid(row=3, column=0, padx=5, pady=5)
 Button(tool_bar, text="Insert Image2",command = lambda: img_selector(2)).grid(row=4, column=0, padx=5, pady=5)
