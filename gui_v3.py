fa924ae8d758366c98f9f1dbabc1eab453b94aea
mpierangeli <mpierangeli@estudiantes.unsam.edu.ar>
2022-03-08 23:34:05 -0300

    update gui_v3

--- gui_v3.py
+++ gui_v3.py
@@ -7,7 +7,7 @@ def draw_line(event):
     x, y = event.x, event.y
     if cv.old_coords:
         x1, y1 = cv.old_coords
-        cv.create_line(x, y, x1, y1,tags="dibujos",fill="red")
+        cv.create_line(x, y, x1, y1,tags="dibujos",fill="red",width=brushSize.get(),smooth=True,capstyle=ROUND,joinstyle=ROUND)
     cv.old_coords = x,y
 
 def clear_app(event):
@@ -28,21 +28,20 @@ def img_selector(num):
 def zoom_app(event):
     global zoom
     global pepe
-    zoom = round(zoom+event.delta/1200,1)
+
+    zoom = round(zoom+0.1*event.delta/120,1)
     if zoom>2: zoom = 2
-    if zoom<1: zoom = 1
-    zoom_info.set(int(zoom*100))
-    x = cv.canvasx(event.x)
-    y = cv.canvasy(event.y)
-    
-    factor = 1+0.1*event.delta/120
-    #factor = zoom
-    if zoom != 1 and zoom !=2:
-        cv.scale(ALL, 650/2, 600/2, factor, factor)
+    elif zoom<1: zoom = 1
+    else: 
+        factor = 1.001**event.delta
+        #x = cv.canvasx(event.x) 
+        #y = cv.canvasy(event.y)
+        cv.scale(ALL, 650/2, 600/2, factor, factor) # x e y, irian en origins, para zoomear donde apunto...
+    zoom_info.set("Zoom = "+str(int(zoom*100))+"%")
+   
     #pep = Image.open("images\original.png")
     #pepe = ImageTk.PhotoImage(pep.resize(round((pep.width+zoom)),round(pep.height+zoom)))
     #cv.itemconfig(cv_ima,image=pepe)
-    #print (zoom)
 
 #MAIN WINDOW SETUP
 root = Tk()
@@ -70,6 +69,9 @@ b1 = Button(l_frame, text="Clear Canvas",font=("Roboto",12),command = canvas_cle
 b2 = Button(l_frame, text="IMG1",font=("Roboto",12),command = lambda: img_selector(1), relief=FLAT, bg="#555",fg="#FFF",activebackground="#555",activeforeground="#2DD",bd=0,height=2,width=15,justify=CENTER).grid(row=3, column=0,pady=10)
 b3 = Button(l_frame, text="IMG2",font=("Roboto",12),command = lambda: img_selector(2), relief=FLAT, bg="#555",fg="#FFF",activebackground="#555",activeforeground="#2DD",bd=0,height=2,width=15,justify=CENTER).grid(row=4, column=0,pady=10)
 
+brushSize = IntVar(l_frame, value=1)
+brushSlider = Scale(l_frame, from_=1,to=10,variable=brushSize).grid(row=5,column=0)
+
 
 # DIBUJAR EN CANVAS
 
@@ -81,7 +83,7 @@ cv.bind('<B1-Motion>', draw_line)
 cv.bind('<ButtonRelease-1>', clear_app)
 cv.bind("<MouseWheel>", zoom_app)
 
-zoom_info = IntVar(r_frame,value=100)
+zoom_info = StringVar(r_frame,value="Zoom = 100%")
 infolabel = Label(r_frame, textvariable=zoom_info,bg="#222",fg="#FFF",font=("Roboto",12)).grid(row=1,column=0)
 #mm = ImageTk.PhotoImage(Image.open("images\original.png"))
 #cv_ima = cv.create_image(560/2, 560/2, anchor=CENTER, image=mm, #tags="foto")
