f7966fea331b39b56070288418e9f588179b33d5
mpierangeli <mpierangeli@estudiantes.unsam.edu.ar>
2022-04-06 17:27:00 -0300

    agrego roi circular

--- saurus_v0_0.py
+++ saurus_v0_0.py
@@ -30,7 +30,7 @@ def menu_creator():
     
     editmenu = Menu(menubar, tearoff=0)
     editmenu.add_command(label="ROI Rectangular",command=square_gen)
-    editmenu.add_command(label="ROI Circular")
+    editmenu.add_command(label="ROI Circular", command=circle_gen)
     editmenu.add_command(label="Medición",command=ruler_gen)
 
     reportmenu = Menu(menubar, tearoff=0)
@@ -83,16 +83,16 @@ def square_gen():
     root.bind('<ButtonRelease-1>', finish_square)
 def start_square(event):
     global x0, y0
-    cv.delete("temp_square","square","temp_text")
+    cv.delete("temp_square","square","temp_text_s")
     x0, y0 = event.x, event.y
 def temp_square(event):
-    cv.delete("temp_square","temp_text")
+    cv.delete("temp_square","temp_text_s")
     cv.create_line(x0,y0,event.x,y0,fill="#A00",dash=(7,),tags="temp_square")
     cv.create_line(x0,y0,x0,event.y,fill="#A00",dash=(7,),tags="temp_square")
     cv.create_line(event.x,y0,event.x,event.y,fill="#A00",dash=(7,),tags="temp_square")
     cv.create_line(x0,event.y,event.x,event.y,fill="#A00",dash=(7,),tags="temp_square")
-    cv.create_text((event.x+x0)/2,y0-10,text=str(abs(round(px_info_var*(event.x-x0),2)))+"mm",fill="#F00",font=("Roboto", 9),tags="temp_text")
-    cv.create_text(x0-10,(event.y+y0)/2,text=str(abs(round(px_info_var*(event.y-y0),2)))+"mm",fill="#F00",font=("Roboto", 9),tags="temp_text",angle=90)
+    cv.create_text((event.x+x0)/2,y0-10,text=str(abs(round(px_info_var*(event.x-x0),2)))+"mm",fill="#F00",font=("Roboto", 9),tags="temp_text_s")
+    cv.create_text(x0-10,(event.y+y0)/2,text=str(abs(round(px_info_var*(event.y-y0),2)))+"mm",fill="#F00",font=("Roboto", 9),tags="temp_text_s",angle=90)
 def finish_square(event):
     global x1, y1
     x1, y1 = event.x, event.y
@@ -107,6 +107,39 @@ def finish_square(event):
     root.unbind('<B1-Motion>')
     root.unbind('<ButtonRelease-1>')
 
+def circle_gen():
+    root.config(cursor="tcross")
+    root.bind('<Button-1>', start_circle)
+    root.bind('<B1-Motion>', temp_circle)
+    root.bind('<ButtonRelease-1>', finish_circle)
+def start_circle(event):
+    global x0, y0
+    cv.delete("temp_circle","circle","temp_text")
+    x0, y0 = event.x, event.y
+def temp_circle(event):
+    cv.delete("temp_circle","temp_text_c")
+    dx = abs(event.x-x0)
+    dy = abs(event.y-y0)
+    r = math.sqrt(dx**2+dy**2)
+    cv.create_oval(x0-r,y0-r,x0+r,y0+r,outline="#A00",dash=(7,),tags="temp_circle")
+    cv.create_text(x0,y0+r+10,text="r: "+str(abs(round(px_info_var*r,2)))+"mm",fill="#F00",font=("Roboto", 9),tags="temp_text_c")
+def finish_circle(event):
+    global x1, y1
+    x1, y1 = event.x, event.y
+    dx = abs(x1-x0)
+    dy = abs(y1-y0)
+    r = math.sqrt(dx**2+dy**2)
+    if x1 == x0 or y1 == y0:
+        print("NO SUELTE EL MOUSE")
+        return
+    cv.create_oval(x0-r,y0-r,x0+r,y0+r,outline="#F00",tags="circle")
+    cv.delete("temp_circle")
+    cv.old_coords = None
+    root.config(cursor="arrow")
+    root.unbind('<Button-1>')
+    root.unbind('<B1-Motion>')
+    root.unbind('<ButtonRelease-1>')
+
 def ruler_gen():
     root.config(cursor="tcross")
     root.bind('<Button-1>', start_ruler)
@@ -114,11 +147,11 @@ def ruler_gen():
     root.bind('<ButtonRelease-1>', finish_ruler)
 def start_ruler(event):
     global x0, y0
-    cv.delete("temp_ruler","medicion","temp_text")
+    cv.delete("temp_ruler","medicion","temp_text_r")
     x0, y0 = event.x, event.y
 def temp_ruler(event):
     
-    cv.delete("temp_ruler","temp_text")
+    cv.delete("temp_ruler","temp_text_r")
     cv.create_line(x0,y0,event.x,event.y,fill="#1BB",dash=(3,),tags="temp_ruler")
     dx = event.x-x0
     dy = event.y-y0
@@ -131,7 +164,7 @@ def temp_ruler(event):
         ang = -ang
         a -= 20
 
-    cv.create_text((event.x+x0)/2+a,(event.y+y0)/2+b,text=str(abs(round(px_info_var*(math.sqrt(dx**2+dy**2)),1)))+"mm",fill="#2CC",font=("Roboto", 9),tags="temp_text",angle=ang)
+    cv.create_text((event.x+x0)/2+a,(event.y+y0)/2+b,text=str(abs(round(px_info_var*(math.sqrt(dx**2+dy**2)),1)))+"mm",fill="#2CC",font=("Roboto", 9),tags="temp_text_r",angle=ang)
 def finish_ruler(event):
     global x1, y1
     x1, y1 = event.x, event.y
