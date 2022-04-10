49c014328be411373fee2ae84b781f7daa8486ba
mpierangeli <mpierangeli@estudiantes.unsam.edu.ar>
2022-04-10 18:20:18 -0300

    inicio de funcionalidad de zoom y corrección de bugs

--- saurus_v0_0.py
+++ saurus_v0_0.py
@@ -5,6 +5,7 @@ import pydicom
 import cv2
 import numpy as np
 import math
+import imutils
 
 
 def windows_creator():
@@ -56,16 +57,16 @@ def canvas_creator():
     global cv1,cv2,cv3,cv4
     CV_W.set(int(MF_W.get()/2)-5)
     CV_H.set(int(MF_H.get()/2)-5)
-    cv1 = Canvas(main_frame, width=CV_W.get(),height=CV_H.get(),bg="#666",highlightthickness=0)
+    cv1 = Canvas(main_frame, width=CV_W.get(),height=CV_H.get(),bg="#000",highlightthickness=0)
     cv1.grid(row=0,column=0,padx=(0,5),pady=(0,5))
     cv1.old_coords = None
-    cv2 = Canvas(main_frame, width=CV_W.get(),height=CV_H.get(),bg="#666",highlightthickness=0)
+    cv2 = Canvas(main_frame, width=CV_W.get(),height=CV_H.get(),bg="#000",highlightthickness=0)
     cv2.grid(row=0,column=1,padx=(5,0),pady=(0,5))
     cv2.old_coords = None
-    cv3 = Canvas(main_frame, width=CV_W.get(),height=CV_H.get(),bg="#666",highlightthickness=0)
+    cv3 = Canvas(main_frame, width=CV_W.get(),height=CV_H.get(),bg="#000",highlightthickness=0)
     cv3.grid(row=1,column=0,padx=(0,5),pady=(5,0))
     cv3.old_coords = None
-    cv4 = Canvas(main_frame, width=CV_W.get(),height=CV_H.get(),bg="#666",highlightthickness=0)
+    cv4 = Canvas(main_frame, width=CV_W.get(),height=CV_H.get(),bg="#000",highlightthickness=0)
     cv4.grid(row=1,column=1,padx=(5,0),pady=(5,0))
     cv4.old_coords = None
 
@@ -73,6 +74,10 @@ def canvas_creator():
     cv2.bind("<Enter>",lambda event, arg=cv2: focus_cv(event,arg))
     cv3.bind("<Enter>",lambda event, arg=cv3: focus_cv(event,arg))
     cv4.bind("<Enter>",lambda event, arg=cv4: focus_cv(event,arg))
+    cv1.bind("<Leave>",lambda event, arg=cv1: unfocus_cv(event,arg))
+    cv2.bind("<Leave>",lambda event, arg=cv2: unfocus_cv(event,arg))
+    cv3.bind("<Leave>",lambda event, arg=cv3: unfocus_cv(event,arg))
+    cv4.bind("<Leave>",lambda event, arg=cv4: unfocus_cv(event,arg))
 
     cv1.bind("<MouseWheel>", slice_selection)
     cv3.bind("<MouseWheel>", slice_selection)  
@@ -83,7 +88,11 @@ def canvas_creator():
 def focus_cv(event,arg):
     global cv
     cv = arg
-
+    cv.create_text(50,CV_H.get()-20,text="FOCUSED",font=("Roboto",5),fill="#FFF",tag="focus_check")
+def unfocus_cv(event,arg):
+    global cv
+    cv = arg
+    cv.delete("focus_check")
 def slice_selection(event):
     global slice_num,depth_num
     if event.delta > 0 and slice_num < len(axiales)-1: slice_num+=1
@@ -135,10 +144,9 @@ def patient_loader():
 
 def info_tab_gen(event):
     global info_tab
-    info_tab = Frame(root, width=500, height=MF_H.get(), background="#222")
-    info_tab.place(relx=0,rely=0)
-    info_tab.pack_propagate(0)
-    l2 = Label(info_tab, text="INFO",bg="#222",font=("Roboto",10),fg="#FFF").grid(row=0,column=0,pady=(10,20))
+    info_tab = Frame(root,background="#222")
+    info_tab.place(relx=0,rely=0, width=200, height=MF_H.get())
+    l2 = Label(info_tab, text="INFORMACIÓN",bg="#222",font=("Roboto",10),fg="#FFF").grid(row=0,column=0,pady=(10,20))
 
     #IMPORTANT DATA
     # VER Q MOSTRAR EN EL PANEL DE INFO!!
@@ -162,14 +170,22 @@ def info_tab_destroy(event):
     root.bind("<F1>",info_tab_gen)
 def set_img(num,depth):
     global cv,cv1,cv2,cv3,cv4,axial_t2,coronal_t2
-    axial_t2 = ImageTk.PhotoImage(image=Image.fromarray(axiales[num]))
-    coronal_t2 = ImageTk.PhotoImage(image=Image.fromarray(coronales[depth]))
+
+    temp_axial_t2 = imutils.resize(axiales[num], height=CV_H.get())
+    temp_coronal_t2 = imutils.resize(coronales[depth], width=temp_axial_t2.shape[0])
+    axial_t2 = ImageTk.PhotoImage(Image.fromarray(temp_axial_t2))
+    coronal_t2 = ImageTk.PhotoImage(Image.fromarray(temp_coronal_t2))
+    #axial_t2 = ImageTk.PhotoImage(Image.fromarray(axiales[num]))
+    #coronal_t2 = ImageTk.PhotoImage(Image.fromarray(coronales[depth]))
     
     cv1.create_image(CV_W.get()/2, CV_H.get()/2, anchor=CENTER, image=axial_t2, tags="axial_t2")
     cv2.create_image(CV_W.get()/2, CV_H.get()/2, anchor=CENTER, image=coronal_t2, tags="coronal_t2")
     cv2.delete("slice_marker")
     cv2.create_line(CV_W.get()/2-coronal_t2.width()/2, num*factor+CV_H.get()/2-coronal_t2.height()/2, coronal_t2.width()/2+CV_W.get()/2, num*factor+CV_H.get()/2-coronal_t2.height()/2, fill="#2DD", tags="slice_marker")
     cv2.create_line(CV_W.get()/2-coronal_t2.width()/2, (num+1)*factor+CV_H.get()/2-coronal_t2.height()/2, coronal_t2.width()/2+CV_W.get()/2, (num+1)*factor+CV_H.get()/2-coronal_t2.height()/2, fill="#2DD", tags="slice_marker")
+    cv2.delete("cv2_info")
+    cv2.create_text(80,20,text="Axial/height: "+str(num+1),fill="#2CC",font=("Roboto", 12),tags="cv2_info")
+    cv2.create_text(80,40,text="Coronal/depth: "+str(depth+1),fill="#2CC",font=("Roboto", 12),tags="cv2_info")
 
 ## HERRAMIENTAS
 def square_gen():
@@ -179,7 +195,7 @@ def square_gen():
     root.bind('<ButtonRelease-1>', finish_square)
 def start_square(event):
     global x0, y0
-    cv.delete("temp_square","square","temp_text_s")
+    #cv.delete("temp_square","square","temp_text_s")
     x0, y0 = event.x, event.y
 def temp_square(event):
     cv.delete("temp_square","temp_text_s")
@@ -218,16 +234,16 @@ def circle_gen():
     root.bind('<ButtonRelease-1>', finish_circle)
 def start_circle(event):
     global x0, y0
-    cv.delete("temp_circle","circle","temp_text")
+    #cv.delete("temp_circle","circle","temp_text")
     x0, y0 = event.x, event.y
 def temp_circle(event):
     cv.delete("temp_circle","temp_text_c")
     dx = abs(event.x-x0)
     dy = abs(event.y-y0)
     r = math.sqrt(dx**2+dy**2)
+    r_real = math.sqrt((dx*px_info_var[0])**2+(dy*px_info_var[1])**2) # Porq distancia real depende del ancho de pixel en cada dirección.
     cv.create_oval(x0-r,y0-r,x0+r,y0+r,outline="#A00",dash=(7,),tags="temp_circle")
-    cv.create_text(x0,y0+r+10,text="r: "+str(abs(round(px_info_var[0]*r,2)))+"mm",fill="#F00",font=("Roboto", 9),tags="temp_text_c")
-    ## VER ACA EL PX_INFO_VAR
+    cv.create_text(x0,y0+r+10,text="r: "+str(round(r_real,2))+"mm",fill="#F00",font=("Roboto", 9),tags="temp_text_c")
 def finish_circle(event):
     global x1, y1
     x1, y1 = event.x, event.y
@@ -252,7 +268,7 @@ def ruler_gen():
     root.bind('<ButtonRelease-1>', finish_ruler)
 def start_ruler(event):
     global x0, y0
-    cv.delete("temp_ruler","medicion","temp_text_r")
+    #cv.delete("temp_ruler","medicion","temp_text_r")
     x0, y0 = event.x, event.y
 def temp_ruler(event):
     
@@ -268,9 +284,8 @@ def temp_ruler(event):
     elif dx*dy > 0: 
         ang = -ang
         a -= 20
-
-    cv.create_text((event.x+x0)/2+a,(event.y+y0)/2+b,text=str(abs(round(px_info_var[0]*(math.sqrt(dx**2+dy**2)),1)))+"mm",fill="#2CC",font=("Roboto", 9),tags="temp_text_r",angle=ang)
-    #VER ACA EL PIXEL_INFO_VAR
+    r_real = math.sqrt((dx*px_info_var[0])**2+(dy*px_info_var[1])**2)
+    cv.create_text((event.x+x0)/2+a,(event.y+y0)/2+b,text=str(round(r_real,2))+"mm",fill="#2CC",font=("Roboto", 9),tags="temp_text_r",angle=ang)
 def finish_ruler(event):
     global x1, y1
     x1, y1 = event.x, event.y
